fd450ff29813cf317a1801e3bb28e90177ad5918
Matthias Gutsch <matthias.gutsch@gmail.com>
2026-06-04 22:04:03 +0200

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import {Component} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { SnelloChatWidgetComponent } from './pages/snello-chat-widget/snello-chat-widget.component';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { SnelloChatWidgetComponent } from "./pages/snello-chat-widget/snello-chat-widget.component";
+import { LucideAngularModule } from "@lucide/angular";
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    imports: [RouterOutlet, SnelloChatWidgetComponent]
+  selector: "app-root",
+  standalone: true,
+  templateUrl: "./app.component.html",
+  imports: [RouterOutlet, SnelloChatWidgetComponent, LucideAngularModule],
 })
-export class AppComponent {
-}
+export class AppComponent {}
